74d3b1ef216e1e0e7ec9eccc766546b180428141
unknown <joaozinhoalberto@gmail.comm>
2020-11-18 12:59:51 -0300

    finixi rim

--- src/pages/apidragon/apidragonform.js
+++ src/pages/apidragon/apidragonform.js
@@ -137,6 +137,7 @@ onCreate(event) {
     }
     fetch('http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon/', request)
         .then(response => response.json())
+        .then( () => { window.history.back() } )
 }
 }
 
